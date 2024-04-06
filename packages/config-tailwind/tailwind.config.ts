7cad9851b139ba7c8ca72f226fce5c4ab1cae568
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-04-06 22:10:02 +0800

    refac: remove templates

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,15 +1,7 @@
 import type { Config } from "tailwindcss";
 
 // We want each package to be responsible for its own content.
-const config: Omit<Config, "content"> = {
-  theme: {
-    extend: {
-      backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
-      },
-    },
-  },
-  plugins: [],
-};
+/** @type { import('tailwindcss').Config } */
+const config: Omit<Config, "content"> = {};
+
 export default config;
